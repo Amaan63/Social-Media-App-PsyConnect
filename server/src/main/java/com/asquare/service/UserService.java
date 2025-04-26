c9679199732fb54e651213a830a0b72add7629f9
Amaan63 <amaansayed63@gmail.com>
2025-04-26 13:05:57 +0530

    User Service Interface is created

---
+++ server/src/main/java/com/asquare/service/UserService.java
@@ -0,0 +1,20 @@
+package com.asquare.service;
+
+import java.util.List;
+
+import com.asquare.models.User;
+
+public interface UserService {
+  
+  public User registerUser(User user);
+
+  public User findUserById(Integer userId);
+
+  public User findUserByEmail(String email);
+
+  public User followUser(Integer userId1 , Integer userId2);
+
+  public User updateuser(User user);
+
+  public List<User> searchuser(String query);
+}
