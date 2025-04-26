9832c1837c2148f7690161658cd045465fec3d2f
Amaan63 <amaansayed63@gmail.com>
2025-04-26 12:17:56 +0530

    server initialized

---
+++ server/src/main/java/com/asquare/PsyConnectApplication.java
@@ -0,0 +1,13 @@
+package com.asquare;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PsyConnectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PsyConnectApplication.class, args);
+	}
+
+}
