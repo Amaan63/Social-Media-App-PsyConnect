d92b72abbbed2b7cc8fac83d9e172e8797f95f6f
Amaan63 <amaansayed63@gmail.com>
2025-04-26 14:33:15 +0530

    added exception in followUser method

--- server/src/main/java/com/asquare/service/UserService.java
+++ server/src/main/java/com/asquare/service/UserService.java
@@ -5,14 +5,14 @@ import java.util.List;
 import com.asquare.models.User;
 
 public interface UserService {
-  
+
   public User registerUser(User user);
 
   public User findUserById(Integer userId) throws Exception;
 
   public User findUserByEmail(String email);
 
-  public User followUser(Integer userId1 , Integer userId2);
+  public User followUser(Integer userId1, Integer userId2) throws Exception;
 
   public User updateuser(User user);
 
