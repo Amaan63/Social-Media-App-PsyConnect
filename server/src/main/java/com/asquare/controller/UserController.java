363a9cce85c461a4e3bfa5c0bf6cbe568d5b9968
Amaan63 <amaansayed63@gmail.com>
2025-04-26 14:55:59 +0530

    Completed updateUser and getUserById Controller

--- server/src/main/java/com/asquare/controller/UserController.java
+++ server/src/main/java/com/asquare/controller/UserController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
 
 @RestController
 public class UserController {
@@ -23,8 +24,14 @@ public class UserController {
   }
 
   @GetMapping("/users/{userId}")
-  public User getUserById(@PathVariable("userId") Integer id) throws Exception {
-    return null;
+  public User getUserById(@PathVariable("userId") Integer userId) throws Exception {
+    User user = userService.findUserById(userId);
+    return user;
   }
 
+  @PutMapping("/user/{id}")
+  public User updateUser(@RequestBody User user, @PathVariable Integer userId) throws Exception {
+    User updatedUser = userService.updateuser(user, userId);
+    return updatedUser;
+  }
 }
