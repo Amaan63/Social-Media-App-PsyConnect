ede49aaef3b5e4171f060c573f6bdaadc0349ace
Amaan63 <amaansayed63@gmail.com>
2025-04-26 13:18:31 +0530

    controller is created to creae new users

--- server/src/main/java/com/asquare/controller/UserController.java
+++ server/src/main/java/com/asquare/controller/UserController.java
@@ -1,8 +1,23 @@
 package com.asquare.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.asquare.models.User;
+import com.asquare.service.UserService;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
 @RestController
 public class UserController {
-  
+
+  @Autowired
+  UserService userService;
+
+  @PostMapping("/users")
+  public User createUser(@RequestBody User user) {
+    User savedUser = userService.registerUser(user);
+    return savedUser;
+  }
+
 }
