238f3af0291917edf519aab21e9797f5a14567c3
Amaan63 <amaansayed63@gmail.com>
2025-04-26 13:09:48 +0530

    Added Lombok Annotations

--- server/src/main/java/com/asquare/models/User.java
+++ server/src/main/java/com/asquare/models/User.java
@@ -2,8 +2,14 @@ package com.asquare.models;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class User {
 
   @Id
