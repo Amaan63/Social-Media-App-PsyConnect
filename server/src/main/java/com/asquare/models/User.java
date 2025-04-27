89f4be54b6fa6e90783462cdfa58567947c6e7c3
Amaan63 <amaansayed63@gmail.com>
2025-04-27 11:38:56 +0530

    Added Auto Generated ID

--- server/src/main/java/com/asquare/models/User.java
+++ server/src/main/java/com/asquare/models/User.java
@@ -4,6 +4,8 @@ import java.util.ArrayList;
 import java.util.List;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -16,6 +18,7 @@ import lombok.NoArgsConstructor;
 public class User {
 
   @Id
+  @GeneratedValue(strategy = GenerationType.AUTO)
   private Integer id;
   private String firstName;
   private String lastName;
