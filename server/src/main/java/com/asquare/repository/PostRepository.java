330b53b1d8fc1007a85b1bef9b1e466f611ceeee
Amaan63 <amaansayed63@gmail.com>
2025-04-27 12:17:17 +0530

    Added method to find Post by User Id

--- server/src/main/java/com/asquare/repository/PostRepository.java
+++ server/src/main/java/com/asquare/repository/PostRepository.java
@@ -1,9 +1,14 @@
 package com.asquare.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import com.asquare.models.Post;
 
 public interface PostRepository extends JpaRepository<Post, Integer> {
 
+  @Query("select p from Post p where p.user.id=:userId")
+  List<Post> findPostByUserID(Integer userId);
 }
