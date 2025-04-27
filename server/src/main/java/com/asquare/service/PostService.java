bab4ad65de603887e917361238cfbf927c1f4407
Amaan63 <amaansayed63@gmail.com>
2025-04-27 12:27:24 +0530

    Added Exception for likePost method

--- server/src/main/java/com/asquare/service/PostService.java
+++ server/src/main/java/com/asquare/service/PostService.java
@@ -18,5 +18,5 @@ public interface PostService {
 
   Post savedPost(Integer postId, Integer userId);
 
-  Post likedPost(Integer postId, Integer userId);
+  Post likedPost(Integer postId, Integer userId) throws Exception;
 }
