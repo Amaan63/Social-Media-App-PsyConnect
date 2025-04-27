9d2551ca69a1c9b19d552dfd7005327847c8ff76
Amaan63 <amaansayed63@gmail.com>
2025-04-27 14:15:34 +0530

    Added Relation for user and liked

--- server/src/main/java/com/asquare/models/Post.java
+++ server/src/main/java/com/asquare/models/Post.java
@@ -8,6 +8,8 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -21,10 +23,18 @@ public class Post {
   @Id
   @GeneratedValue(strategy = GenerationType.AUTO)
   private Integer id;
+
   private String caption;
+
   private String image;
+
   private String video;
+
+  @ManyToOne
   private User user;
+
+  @OneToMany
   private List<User> liked = new ArrayList<>();
+
   private LocalDateTime createdAt;
 }
