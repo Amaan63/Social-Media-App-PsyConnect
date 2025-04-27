fea437fd27691aa5f6f05acd8070f4c092535a8b
Amaan63 <amaansayed63@gmail.com>
2025-04-27 13:12:20 +0530

    deletePost controller is Added

--- server/src/main/java/com/asquare/controller/PostController.java
+++ server/src/main/java/com/asquare/controller/PostController.java
@@ -7,8 +7,10 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.asquare.models.Post;
+import com.asquare.response.ApiResponse;
 import com.asquare.service.PostService;
 
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -18,10 +20,24 @@ public class PostController {
   @Autowired
   PostService postService;
 
-  @PostMapping("/post/user/{userId}")
+  @PostMapping("/posts/user/{userId}")
   public ResponseEntity<Post> createPost(
-      @RequestBody Post post, @PathVariable Integer userId) throws Exception {
+      @RequestBody Post post,
+      @PathVariable Integer userId) throws Exception {
     Post createdPost = postService.createNewPost(post, userId);
     return new ResponseEntity<>(createdPost, HttpStatus.ACCEPTED);
   }
+
+  @DeleteMapping("/posts/{postId}/{userId}")
+  public ResponseEntity<ApiResponse> deletePost(@PathVariable Integer postId, @PathVariable Integer userId) {
+    try {
+      String message = postService.deletePost(postId, userId);
+      ApiResponse res = new ApiResponse(message, true); // Agar sab sahi hai to success
+      return new ResponseEntity<>(res, HttpStatus.OK);
+    } catch (Exception e) {
+      ApiResponse res = new ApiResponse(e.getMessage(), false); // Agar koi exception hai to fail
+      return new ResponseEntity<>(res, HttpStatus.BAD_REQUEST); // 400 error bhejo
+    }
+  }
+
 }
