e948f26e4362ab6d6dcf0bd09f25b511be7c63ff
Amaan63 <amaansayed63@gmail.com>
2025-04-27 13:23:51 +0530

    savePostController is Created

--- server/src/main/java/com/asquare/controller/PostController.java
+++ server/src/main/java/com/asquare/controller/PostController.java
@@ -15,6 +15,7 @@ import com.asquare.service.PostService;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -60,6 +61,14 @@ public class PostController {
   public ResponseEntity<List<Post>> findAllPostController() {
     List<Post> posts = postService.findAllPost();
     return new ResponseEntity<List<Post>>(posts, HttpStatus.OK);
+  }
+
+  @PutMapping("/posts/{postId}/user/{userId}")
+  public ResponseEntity<Post> savePostController(
+      @PathVariable Integer postId,
+      @PathVariable Integer userId) throws Exception {
+    Post post = postService.savedPost(postId, userId);
+    return new ResponseEntity<Post>(post, HttpStatus.ACCEPTED);
   }
 
 }
