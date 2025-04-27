cf1a0f5c54a7c56e827665880911b5b1749331d1
Amaan63 <amaansayed63@gmail.com>
2025-04-27 11:26:55 +0530

    Added Gender Search in the Search User

--- server/src/main/java/com/asquare/repository/UserRepository.java
+++ server/src/main/java/com/asquare/repository/UserRepository.java
@@ -11,6 +11,6 @@ import com.asquare.models.User;
 public interface UserRepository extends JpaRepository<User, Integer> {
   public User findByEmail(String email);
 
-  @Query("SELECT u FROM User u WHERE u.firstName LIKE %:query% OR u.lastName LIKE %:query% OR u.email LIKE %:query%")
+  @Query("SELECT u FROM User u WHERE u.firstName LIKE %:query% OR u.lastName LIKE %:query% OR u.email LIKE %:query% OR u.gender LIKE %:query%")
   public List<User> searchUser(@Param("query") String query);
 }
