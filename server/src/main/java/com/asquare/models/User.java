35d3ef9c8af41cfbd3342ecaf65a1faa8c26fd80
Amaan63 <amaansayed63@gmail.com>
2025-04-27 14:15:48 +0530

    Added relation for saved post

--- server/src/main/java/com/asquare/models/User.java
+++ server/src/main/java/com/asquare/models/User.java
@@ -7,6 +7,7 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.ManyToMany;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -20,12 +21,21 @@ public class User {
   @Id
   @GeneratedValue(strategy = GenerationType.AUTO)
   private Integer id;
+
   private String firstName;
+
   private String lastName;
+
   private String email;
+
   private String password;
+
   private String gender;
+
   private List<Integer> followers = new ArrayList<>();
+
   private List<Integer> followings = new ArrayList<>();
+
+  @ManyToMany
   private List<Post> savedPost = new ArrayList<>();
 }
