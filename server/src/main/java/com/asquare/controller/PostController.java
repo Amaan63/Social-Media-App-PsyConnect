3abf6a41d8bcea6b6c3985f97bb19452112fee24
Amaan63 <amaansayed63@gmail.com>
2025-04-27 13:15:51 +0530

    findPostByIdController method is Added

--- server/src/main/java/com/asquare/controller/PostController.java
+++ server/src/main/java/com/asquare/controller/PostController.java
@@ -14,6 +14,8 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @RestController
 public class PostController {
@@ -38,6 +40,12 @@ public class PostController {
       ApiResponse res = new ApiResponse(e.getMessage(), false); // Agar koi exception hai to fail
       return new ResponseEntity<>(res, HttpStatus.BAD_REQUEST); // 400 error bhejo
     }
+  }
+
+  @GetMapping("/posts/{postId}")
+  public ResponseEntity<Post> findPostByIdController(@PathVariable Integer postId) throws Exception {
+    Post post = postService.findPostById(postId);
+    return new ResponseEntity<Post>(post, HttpStatus.ACCEPTED);
   }
 
 }
