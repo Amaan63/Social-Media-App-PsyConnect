0e8602da62881e17b62373561b4c776627fd5780
Amaan63 <amaansayed63@gmail.com>
2025-04-27 12:43:13 +0530

    Created Post Controller

---
+++ server/src/main/java/com/asquare/controller/PostController.java
@@ -0,0 +1,11 @@
+package com.asquare.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.asquare.service.PostService;
+
+@RestController
+public class PostController {
+
+}
