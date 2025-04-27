6d16472c4077204004eb822935b2c19a2135b929
Amaan63 <amaansayed63@gmail.com>
2025-04-27 19:57:51 +0530

    Created AppConfig to Configure Spring Security

---
+++ server/src/main/java/com/asquare/config/AppConfig.java
@@ -0,0 +1,15 @@
+package com.asquare.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class AppConfig {
+
+  SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+    return http.build();
+  }
+}
