36ef09ee4f2b62d6ae6d236e5a41f371a7c33da6
Amaan63 <amaansayed63@gmail.com>
2025-04-27 11:39:10 +0530

    Post Entity in Created

---
+++ server/src/main/java/com/asquare/models/Post.java
@@ -0,0 +1,27 @@
+package com.asquare.models;
+
+import java.time.LocalDateTime;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Post {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.AUTO)
+  private Integer id;
+  private String caption;
+  private String image;
+  private String video;
+  private User user;
+  private LocalDateTime createdAt;
+}
