3510444652bffae79db1721c1fb1478f34d400ef
Amaan63 <amaansayed63@gmail.com>
2025-04-28 14:40:35 +0530

    Create User controller method is created

--- server/src/main/java/com/asquare/controller/AuthController.java
+++ server/src/main/java/com/asquare/controller/AuthController.java
@@ -1,6 +1,9 @@
 package com.asquare.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,9 +17,21 @@ public class AuthController {
   @Autowired
   private UserService userService;
 
-  @PostMapping("/public/users")
-  public User createUser(@RequestBody User user) {
-    User savedUser = userService.registerUser(user);
-    return savedUser;
+  // Endpoint to create a new user
+  @PostMapping("/public/users/signUp")
+  public ResponseEntity<Object> createUser(@RequestBody User user) {
+    try {
+      // Check if the email already exists using the service method
+      userService.checkIfUserExistsByEmail(user.getEmail());
+
+      // If email doesn't exist, proceed to register the new user
+      User newUser = userService.registerUser(user);
+
+      // Return the newly created user
+      return new ResponseEntity<>(newUser, HttpStatus.OK);
+    } catch (Exception e) {
+      // Return a bad request response with the exception message if an error occurs
+      return new ResponseEntity<>(e.getMessage(), HttpStatus.CONFLICT);
+    }
   }
 }
