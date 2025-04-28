b2ada78e91b17d47e1dc1ef878f465ffb9a9f91d
Amaan63 <amaansayed63@gmail.com>
2025-04-28 10:08:06 +0530

    Implement basic Spring Security; permit all public paths and require auth for private.

--- server/src/main/java/com/asquare/config/AppConfig.java
+++ server/src/main/java/com/asquare/config/AppConfig.java
@@ -1,15 +1,24 @@
 package com.asquare.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
 public class AppConfig {
 
-  SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+  @Bean // Compulsory to Annotate with Bean
+  public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+    http.sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+        .csrf(csf -> csf.disable())
+        .authorizeHttpRequests(authorize -> authorize
+            .requestMatchers("/public/**").permitAll()// Allow POST /public to be publicly accessible for creating user
+            .requestMatchers("/private/**").authenticated()// Block all the /private and require authentication
+            .anyRequest().permitAll());
     return http.build();
   }
 }
