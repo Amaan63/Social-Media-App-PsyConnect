72f14e5193e3bb30d5e238ea73bee0a991e49343
Amaan63 <amaansayed63@gmail.com>
2025-04-28 14:18:23 +0530

    Added CreateUserController Method

--- server/src/main/java/com/asquare/controller/AuthController.java
+++ server/src/main/java/com/asquare/controller/AuthController.java
@@ -1,8 +1,22 @@
 package com.asquare.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.asquare.models.User;
+import com.asquare.service.UserService;
 
 @RestController
 public class AuthController {
 
+  @Autowired
+  private UserService userService;
+
+  @PostMapping("/public/users")
+  public User createUser(@RequestBody User user) {
+    User savedUser = userService.registerUser(user);
+    return savedUser;
+  }
 }
