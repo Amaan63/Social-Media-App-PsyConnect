8f1cc0a58a87eb73a2056de3bd466250a08622db
Amaan63 <amaansayed63@gmail.com>
2025-04-28 19:12:03 +0530

    Auth Response is Created

---
+++ server/src/main/java/com/asquare/response/AuthResponse.java
@@ -0,0 +1,13 @@
+package com.asquare.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class AuthResponse {
+  private String token;
+  private String message;
+}
