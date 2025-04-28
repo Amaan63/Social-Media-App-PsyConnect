089812bb45854a1032d04255fd91603d9dac3108
Amaan63 <amaansayed63@gmail.com>
2025-04-28 11:36:32 +0530

    Created JWT Constants

---
+++ server/src/main/java/com/asquare/config/JwtConstant.java
@@ -0,0 +1,6 @@
+package com.asquare.config;
+
+public class JwtConstant {
+  public static String JWT_HEADER = "Authorization";
+  public static String SECRET_KEY = "ajsnck3jkcmx0mBXnc1hsjakas7xmcbmzpaga5jnsjabbdj7";
+}
