d6eb47f584f83226add181fa81da2032a3db7f98
Amaan63 <amaansayed63@gmail.com>
2025-04-28 10:34:49 +0530

    Refactor security setup: use httpBasic with defaults and remove stateless config

--- server/src/main/java/com/asquare/config/AppConfig.java
+++ server/src/main/java/com/asquare/config/AppConfig.java
@@ -2,6 +2,7 @@ package com.asquare.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -13,12 +14,13 @@ public class AppConfig {
 
   @Bean // Compulsory to Annotate with Bean
   public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-    http.sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
-        .csrf(csf -> csf.disable())
+    http.csrf(csf -> csf.disable())
         .authorizeHttpRequests(authorize -> authorize
             .requestMatchers("/public/**").permitAll()// Allow POST /public to be publicly accessible for creating user
             .requestMatchers("/private/**").authenticated()// Block all the /private and require authentication
-            .anyRequest().permitAll());
+            .anyRequest().permitAll())
+        .httpBasic(Customizer.withDefaults());
+
     return http.build();
   }
 }
