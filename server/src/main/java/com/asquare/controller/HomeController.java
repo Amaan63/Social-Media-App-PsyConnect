653bacfbf9af0f396c1f231abec7cb3c4ec6d47d
Amaan63 <amaansayed63@gmail.com>
2025-04-28 14:16:26 +0530

    Home Controller method is created

--- server/src/main/java/com/asquare/controller/HomeController.java
+++ server/src/main/java/com/asquare/controller/HomeController.java
@@ -1,8 +1,13 @@
 package com.asquare.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HomeController {
-  
+
+  @GetMapping("/public/home")
+  public String homeControllerHandler() {
+    return "this is home controller";
+  }
 }
