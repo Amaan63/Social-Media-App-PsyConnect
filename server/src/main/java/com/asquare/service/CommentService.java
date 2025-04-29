640633eab069c111775199474b3fee19debc3e5a
Amaan63 <amaansayed63@gmail.com>
2025-04-29 18:37:16 +0530

    Added Exception in findCommentById method

--- server/src/main/java/com/asquare/service/CommentService.java
+++ server/src/main/java/com/asquare/service/CommentService.java
@@ -6,7 +6,7 @@ public interface CommentService {
 
   public Comment createComments(Comment comment, Integer postId, Integer userId) throws Exception;
 
-  public Comment findCommentById(Integer commentId);
+  public Comment findCommentById(Integer commentId) throws Exception;
 
   public Comment likeComment(Integer commentId, Integer userId);
 }
