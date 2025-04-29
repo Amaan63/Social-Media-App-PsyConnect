9da80f082d3fbdc6bc16620e54502408fc1f263c
Amaan63 <amaansayed63@gmail.com>
2025-04-29 15:11:34 +0530

    Created CommentService

---
+++ server/src/main/java/com/asquare/service/CommentService.java
@@ -0,0 +1,12 @@
+package com.asquare.service;
+
+import com.asquare.models.Comment;
+
+public interface CommentService {
+
+  public Comment createComments(Comment comment, Integer postId, Integer userId) throws Exception;
+
+  public Comment findCommentById(Integer commentId);
+
+  public Comment likeComment(Integer commentId, Integer userId);
+}
