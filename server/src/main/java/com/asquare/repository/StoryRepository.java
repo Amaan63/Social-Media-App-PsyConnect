212d55a2a7fe11c3b0ad75d18e23b6e2e98fdf7d
Amaan63 <amaansayed63@gmail.com>
2025-04-29 22:16:45 +0530

    Created Story Repository

---
+++ server/src/main/java/com/asquare/repository/StoryRepository.java
@@ -0,0 +1,13 @@
+package com.asquare.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.asquare.models.Story;
+
+public interface StoryRepository extends JpaRepository<Story, Integer> {
+
+  public List<Story> findByUserId(Integer userId);
+
+}
