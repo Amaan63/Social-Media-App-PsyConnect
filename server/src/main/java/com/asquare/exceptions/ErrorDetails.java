3488df3f935b81b178b53ca3ecc9897ec96013bb
Amaan63 <amaansayed63@gmail.com>
2025-04-30 18:14:58 +0530

    Created error Detail DTO

---
+++ server/src/main/java/com/asquare/exceptions/ErrorDetails.java
@@ -0,0 +1,20 @@
+package com.asquare.exceptions;
+
+import java.time.LocalDateTime;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class ErrorDetails {
+
+  private String message;
+
+  private String error;
+
+  private LocalDateTime timeStamp;
+
+}
