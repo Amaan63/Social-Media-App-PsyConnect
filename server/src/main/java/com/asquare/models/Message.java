cf4ef5634106194897df0b1fb75627f0812a66cd
Amaan63 <amaansayed63@gmail.com>
2025-04-30 13:26:01 +0530

    Added jsonIgnore to avaoid circular looping

--- server/src/main/java/com/asquare/models/Message.java
+++ server/src/main/java/com/asquare/models/Message.java
@@ -2,6 +2,8 @@ package com.asquare.models;
 
 import java.time.LocalDateTime;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -28,6 +30,7 @@ public class Message {
   @ManyToOne
   private User user;
 
+  @JsonIgnore
   @ManyToOne
   private Chat chat;
 
