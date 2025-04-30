0c58dc5822ba0105fbac04f72cd5c4fcffe7c26c
Amaan63 <amaansayed63@gmail.com>
2025-04-30 11:31:02 +0530

    Chat Entity is Created

---
+++ server/src/main/java/com/asquare/models/Chat.java
@@ -0,0 +1,35 @@
+package com.asquare.models;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToMany;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Chat {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Integer id;
+
+  private String chat_name;
+
+  private String chat_image;
+
+  @ManyToMany
+  private List<User> users = new ArrayList<>();
+
+  private LocalDateTime createdAt;
+
+}
