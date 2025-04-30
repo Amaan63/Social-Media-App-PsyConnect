b55c96424db3a949e1c1b5b4da327c4a8afd16f3
Amaan63 <amaansayed63@gmail.com>
2025-04-30 12:49:55 +0530

    Updated the CreateChatRequest

--- server/src/main/java/com/asquare/request/CreateChatRequest.java
+++ server/src/main/java/com/asquare/request/CreateChatRequest.java
@@ -1,7 +1,5 @@
 package com.asquare.request;
 
-import com.asquare.models.User;
-
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -11,10 +9,7 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 public class CreateChatRequest {
 
-  // The currently logged-in user
-  private User loggedInUser;
-
   // The user the logged-in user wants to chat with
-  private User targetUser;
+  private Integer targetUser;
 
 }
