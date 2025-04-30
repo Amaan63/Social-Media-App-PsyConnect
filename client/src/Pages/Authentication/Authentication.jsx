e0a4d320954183465dff590cdae32221cddefe96
Amaan63 <amaansayed63@gmail.com>
2025-04-30 22:16:29 +0530

    Created Authentication page with image and Form

--- client/src/Pages/Authentication/Authentication.jsx
+++ client/src/Pages/Authentication/Authentication.jsx
@@ -1,9 +1,38 @@
-import React from 'react'
+import React from "react";
+import { Grid, Card } from "@mui/material";
+import authBg from "../../assets/images/auth/authBg.jpg";
+import Login from "./Login";
 
 const Authentication = () => {
   return (
-    <div>Authentication</div>
-  )
-}
+    <Grid className="min-h-screen flex flex-col md:grid md:grid-cols-12">
+      {/* Image Section */}
+      <Grid className="md:col-span-8 h-60 md:h-auto mt-3">
+        <img
+          src={authBg}
+          alt="Authentication background"
+          className="h-full w-full object-cover"
+        />
+      </Grid>
 
-export default Authentication
+      {/* Form Section */}
+      <Grid className="md:col-span-4 flex items-center justify-center">
+        <div className="px-6 md:px-10 w-full">
+          <Card className="p-6 md:p-8">
+            <div className="flex flex-col items-center mb-5 space-y-1">
+              <h1 className="logo text-center text-2xl font-semibold">
+                Psy Connect
+              </h1>
+              <p className="text-center text-sm w-full md:w-[70%]">
+                Connecting Lives, Sharing Stories: Your Social World Your Way
+              </p>
+            </div>
+            <Login />
+          </Card>
+        </div>
+      </Grid>
+    </Grid>
+  );
+};
+
+export default Authentication;
