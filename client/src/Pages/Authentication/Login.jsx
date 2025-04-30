79b425b981370c7b7f47e36635e848fc5cb119d1
Amaan63 <amaansayed63@gmail.com>
2025-04-30 22:16:50 +0530

    Created Login form using Formik validations

--- client/src/Pages/Authentication/Login.jsx
+++ client/src/Pages/Authentication/Login.jsx
@@ -1,9 +1,75 @@
-import React from 'react'
+import React, { useState } from "react";
+import { Formik, Form, Field, ErrorMessage } from "formik";
+import { Button, TextField } from "@mui/material";
+import * as Yup from "yup";
+
+const initialValues = {
+  email: "",
+  password: "",
+};
+
+const validationSchema = Yup.object({
+  email: Yup.string().email("Invalid Email").required("Email is Required"),
+  password: Yup.string()
+    .min(6, "Password Must be atleast 6 Characters")
+    .required("Password is Required"),
+});
 
 const Login = () => {
-  return (
-    <div>Login</div>
-  )
-}
+  const [formValue, setFormValue] = useState();
 
-export default Login
+  const handleSubmit = (values) => {
+    console.log("Form submitted", values);
+    setFormValue(values);
+  };
+
+  return (
+    <Formik
+      initialValues={initialValues}
+      validationSchema={validationSchema}
+      onSubmit={handleSubmit}
+    >
+      <Form className="space-y-5">
+        <div>
+          <Field
+            as={TextField}
+            name="email"
+            placeholder="Enter Email"
+            type="email"
+            variant="outlined"
+            fullWidth
+          />
+          <ErrorMessage name="email" component="div" className="text-red-500" />
+        </div>
+
+        <div>
+          <Field
+            as={TextField}
+            name="password"
+            placeholder="Enter Password"
+            type="password"
+            variant="outlined"
+            fullWidth
+          />
+          <ErrorMessage
+            name="password"
+            component="div"
+            className="text-red-500"
+          />
+        </div>
+
+        <Button
+          sx={{ padding: ".8rem 0rem" }}
+          fullWidth
+          type="submit"
+          variant="contained"
+          color="primary"
+        >
+          Login
+        </Button>
+      </Form>
+    </Formik>
+  );
+};
+
+export default Login;
