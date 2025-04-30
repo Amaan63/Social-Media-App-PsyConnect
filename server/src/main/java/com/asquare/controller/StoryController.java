201efc768472cd5251e9795019c06c223db8f7ac
Amaan63 <amaansayed63@gmail.com>
2025-04-30 10:38:07 +0530

    added createStory Controller

---
+++ server/src/main/java/com/asquare/controller/StoryController.java
@@ -0,0 +1,43 @@
+package com.asquare.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.asquare.models.Story;
+import com.asquare.models.User;
+import com.asquare.service.StoryService;
+import com.asquare.service.UserService;
+
+@RestController
+@RequestMapping("/private")
+public class StoryController {
+
+  @Autowired
+  private StoryService storyService;
+
+  @Autowired
+  private UserService userService;
+
+  @PostMapping("/story/createStory")
+  public ResponseEntity<?> createStory(@RequestBody Story story,
+      @RequestHeader("Authorization") String jwt) {
+    try {
+      User user = userService.findUserByJwt(jwt);
+      Story createdStory = storyService.createStory(story, user);
+      return ResponseEntity.ok(createdStory);
+    } catch (Exception e) {
+      return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+          .body("Failed to create story: " + e.getMessage());
+    }
+  }
+}
