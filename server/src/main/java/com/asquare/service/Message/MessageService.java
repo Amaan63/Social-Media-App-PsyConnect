52add416bcb842332a725997cba505bad388d931
Amaan63 <amaansayed63@gmail.com>
2025-04-30 13:32:57 +0530

    Removed Chat and Message is Added in Parameters

--- server/src/main/java/com/asquare/service/Message/MessageService.java
+++ server/src/main/java/com/asquare/service/Message/MessageService.java
@@ -2,12 +2,12 @@ package com.asquare.service.Message;
 
 import java.util.List;
 
-import com.asquare.models.Chat;
 import com.asquare.models.Message;
+import com.asquare.models.User;
 
 public interface MessageService {
 
-  public Message createMessage(Integer userId, Integer chatId, Chat chat) throws Exception;
+  public Message createMessage(User user, Integer chatId, Message message) throws Exception;
 
   public List<Message> findChatsMessages(Integer chatId) throws Exception;
 
