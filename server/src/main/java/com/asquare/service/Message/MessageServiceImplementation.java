b1dd9322f4c2c22e312969388d2e5527f4cf775c
Amaan63 <amaansayed63@gmail.com>
2025-04-30 13:33:10 +0530

    Added createMessage Method

---
+++ server/src/main/java/com/asquare/service/Message/MessageServiceImplementation.java
@@ -0,0 +1,41 @@
+package com.asquare.service.Message;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.asquare.models.Chat;
+import com.asquare.models.Message;
+import com.asquare.models.User;
+import com.asquare.repository.MessageRepository;
+import com.asquare.service.Chat.ChatService;
+
+@Service
+public class MessageServiceImplementation implements MessageService {
+
+  @Autowired
+  private MessageRepository messageRepository;
+
+  @Autowired
+  ChatService chatService;
+
+  @Override
+  public Message createMessage(User user, Integer chatId, Message reqMessage) throws Exception {
+    Message message = new Message();
+    Chat chat = chatService.findChatById(chatId);
+    message.setChat(chat);
+    message.setContent(reqMessage.getContent());
+    message.setImage(reqMessage.getImage());
+    message.setUser(user);
+    message.setCreatedAt(LocalDateTime.now());
+    return messageRepository.save(message);
+  }
+
+  @Override
+  public List<Message> findChatsMessages(Integer chatId) throws Exception {
+    return null;
+  }
+
+}
