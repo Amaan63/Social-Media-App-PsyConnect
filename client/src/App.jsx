99ebe349ec986613bedf88d4bb8c6e0e205884cf
Amaan63 <amaansayed63@gmail.com>
2025-04-30 22:17:09 +0530

    Added AUthentication component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,9 +2,11 @@ import "./App.css";
 import Authentication from "./Pages/Authentication/Authentication";
 
 function App() {
-  return <>
-    <Authentication />
-  </>;
+  return (
+    <>
+      <Authentication />
+    </>
+  );
 }
 
 export default App;
