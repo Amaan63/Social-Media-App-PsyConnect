105829962a54d5b42323e1f3239808cc0456e006
Amaan63 <amaansayed63@gmail.com>
2025-04-30 13:44:50 +0530

    Added createMessage Controller

--- server/src/main/java/com/asquare/controller/MessageController.java
+++ server/src/main/java/com/asquare/controller/MessageController.java
@@ -1,10 +1,33 @@
 package com.asquare.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.asquare.models.Message;
+import com.asquare.models.User;
+import com.asquare.service.Message.MessageService;
+import com.asquare.service.User.UserService;
 
 @RestController
 @RequestMapping("/private")
 public class MessageController {
-  
+
+  @Autowired
+  private MessageService messageService;
+
+  @Autowired
+  private UserService userService;
+
+  @PostMapping("/messages/createMessage/chat/{chatId}")
+  public Message creatMessageController(@RequestBody Message reqMessage, @RequestHeader("Authorization") String jwt,
+      @PathVariable("chatId") Integer chatId) throws Exception {
+    User user = userService.findUserByJwt(jwt);
+    Message message = messageService.createMessage(user, chatId, reqMessage);
+    return message;
+  }
 }
