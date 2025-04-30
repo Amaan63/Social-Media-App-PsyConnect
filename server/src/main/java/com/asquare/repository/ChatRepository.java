bac684c89c341d97884b4fc1ed3533b1cce24b44
Amaan63 <amaansayed63@gmail.com>
2025-04-30 11:40:42 +0530

    add query to find chat between two users by checking membership in chat users list

--- server/src/main/java/com/asquare/repository/ChatRepository.java
+++ server/src/main/java/com/asquare/repository/ChatRepository.java
@@ -3,11 +3,20 @@ package com.asquare.repository;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import com.asquare.models.Chat;
+import com.asquare.models.User;
 
 public interface ChatRepository extends JpaRepository<Chat, Integer> {
 
   public List<Chat> findByUsersId(Integer userId);
+
+  // Custom query to find a Chat that includes both users:
+  // - :user must be a member of c.users
+  // - :reqUser must also be a member of c.users
+  @Query("select c from Chat c where :user Member of c.users And :reqUser Member of c.users")
+  public Chat findChatByUsersId(@Param("user") User user, @Param("reqUser") User reqUser);
 
 }
