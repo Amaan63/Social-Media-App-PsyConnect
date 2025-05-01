20ee68f744dd0376a0ad0ca9671ed436bffe6403
Amaan63 <amaansayed63@gmail.com>
2025-05-01 22:18:20 +0530

    changed the thunk to named import

--- client/src/Redux/store.js
+++ client/src/Redux/store.js
@@ -2,15 +2,15 @@
 import { applyMiddleware, combineReducers, legacy_createStore } from "redux";
 
 // redux-thunk middleware import kar rahe hain, async actions ke liye
-import thunk from "redux-thunk";
+import { thunk } from "redux-thunk"; // ✅ named import
+
 import { authenticationReducer } from "./Authentication/authentication.reducer";
 
 // combineReducers se multiple reducers ko ek sath combine karne ka structure banaya gaya hai
 const rootReducers = combineReducers({
-  // yahan alag-alag reducers ko key ke form mein add kiya ja sakta hai, jaise: user: userReducer
   auth: authenticationReducer,
 });
 
 // legacy_createStore se store banaya ja raha hai
 // applyMiddleware(thunk) ka use async actions (jaise API calls) ke liye kiya jata hai
-export const store = legacy_createStore(rootReducers, applyMiddleware(thunk));
+export const store = legacy_createStore(rootReducers, applyMiddleware(thunk)); // ✅ this now works
