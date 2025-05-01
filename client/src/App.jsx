deb5388e391f43b089c187d1be0a28e3e5d1f7cc
Amaan63 <amaansayed63@gmail.com>
2025-05-01 22:55:19 +0530

    Added Routes to navigate through the website

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,18 @@
+import { Route, Routes } from "react-router-dom";
 import "./App.css";
 import Authentication from "./Pages/Authentication/Authentication";
+import Message from "./Pages/MessagePage/Message";
+import Home from "./Pages/HomePage/Home";
 
 function App() {
   return (
     <>
-      <Authentication />
+      <Routes>
+        <Route path="/home" element={<Home />} />
+        <Route path="/message" element={<Message />} />
+        {/* After / anything will come will go to Authentication */}
+        <Route path="/*" element={<Authentication />} />
+      </Routes>
     </>
   );
 }
