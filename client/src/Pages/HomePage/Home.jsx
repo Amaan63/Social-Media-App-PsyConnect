4a67347b3a6c13af2c998857e7f5bdd3a6b3efca
Amaan63 <amaansayed63@gmail.com>
2025-05-02 12:34:53 +0530

    Added teh SIdebar,Mainfeed , Home Right in proper Grid Format

--- client/src/Pages/HomePage/Home.jsx
+++ client/src/Pages/HomePage/Home.jsx
@@ -1,9 +1,46 @@
-import React from 'react'
+import React from "react";
+import { Grid } from "@mui/material";
+import SideBar from "../../components/SideBar";
+import { Route, Routes, useLocation } from "react-router-dom";
+import MainFeedSection from "../../components/MiddlePart/MainFeedSection";
+import Reels from "../../components/Reels/Reels";
+import CreateReelsForm from "../../components/Reels/CreateReelsForm";
+import Profile from "../Profile/Profile";
+import HomeRight from "../../components/Home Components/HomeRight";
 
 const Home = () => {
+  const location = useLocation();
   return (
-    <div>Home</div>
-  )
-}
+    <div className="px-20">
+      <Grid container spacing={0}>
+        <Grid item xs={0} lg={3}>
+          <div className="sticky top-0">
+            <SideBar />
+          </div>
+        </Grid>
 
-export default Home
+        <Grid
+          item
+          xs={12}
+          lg={location.pathname === "/" ? 6 : 9}
+          className="px-5 flex justify-center"
+        >
+          <Routes>
+            <Route path="/" element={<MainFeedSection />} />
+            <Route path="/reels" element={<Reels />} />
+            <Route path="/create-reels" element={<CreateReelsForm />} />
+            <Route path="/profile/:id" element={<Profile />} />
+          </Routes>
+        </Grid>
+
+        <Grid item xs={0} lg={3} className="relative">
+          <div className="sticky top-0 w-full">
+            <HomeRight />
+          </div>
+        </Grid>
+      </Grid>
+    </div>
+  );
+};
+
+export default Home;
