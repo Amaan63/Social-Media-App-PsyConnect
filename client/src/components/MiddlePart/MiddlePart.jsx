2ac154630989df4f945667df6b77327b167f1063
Amaan63 <amaansayed63@gmail.com>
2025-05-02 17:47:53 +0530

    Created Add new Story icon and displaying multiple Stories

--- client/src/components/MiddlePart/MiddlePart.jsx
+++ client/src/components/MiddlePart/MiddlePart.jsx
@@ -1,7 +1,25 @@
+import { Avatar } from "@mui/material";
 import React from "react";
+import StoryCircle from "./StoryCircle";
+import AddIcon from "@mui/icons-material/Add";
 
+const story = [11, 12, 13, 45, 69, 1];
 const MiddlePart = () => {
-  return <div></div>;
+  return (
+    <div className="px-20">
+      <div className="flex  items-center p-5 rounded-b-md">
+        <div className="flex flex-col items-center mr-4 cursor-pointer">
+          <Avatar sx={{ width: "5rem", height: "5rem" }}>
+            <AddIcon sx={{ fontSize: "3rem" }} />
+          </Avatar>
+          <p>New</p>
+        </div>
+        {story.map((item) => (
+          <StoryCircle />
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default MiddlePart;
