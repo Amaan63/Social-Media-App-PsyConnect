728b5e6a4121717abc5a20e7d0d36c49c0f6a5cb
Amaan63 <amaansayed63@gmail.com>
2025-05-02 14:33:04 +0530

    Used SideBarNavigation and map it to shouw different fields

--- client/src/components/SideBar/SideBar.jsx
+++ client/src/components/SideBar/SideBar.jsx
@@ -1,7 +1,23 @@
 import React from "react";
-
+import { navigationMenu } from "./SideBarNavigation";
 const SideBar = () => {
-  return <div>SideBar</div>;
+  return (
+    <div className="card h-screen flex flex-col justify-between py-5">
+      <div className="space-y-8 pl-5">
+        <div className="">
+          <span className="logo font-bold text-2xl">PsyConnect</span>
+        </div>
+        <div className="space-y-8">
+          {navigationMenu.map((item) => (
+            <div className=" cursor-pointer flex space-x-3 items-center">
+              {item.icon}
+              <p className="text-xl">{item.title}</p>
+            </div>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default SideBar;
