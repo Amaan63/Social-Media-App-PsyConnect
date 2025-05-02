b414602bed3d23c3bd55638a272865a1b791bdc6
Amaan63 <amaansayed63@gmail.com>
2025-05-02 22:57:42 +0530

    Created User Reel Card

---
+++ client/src/components/Reels/UserReelCard.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const UserReelCard = () => {
+  return (
+    <div className="w-[15rem] p-2">
+      <video
+        controls
+        className="w-full h-full"
+        src="https://cdn.pixabay.com/video/2022/07/20/124774-732319023_tiny.mp4"
+      />
+    </div>
+  );
+};
+
+export default UserReelCard;
