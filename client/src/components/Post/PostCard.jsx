fd0cc8184008575674e3ffe75c4f4500c38f010b
Amaan63 <amaansayed63@gmail.com>
2025-05-02 20:16:23 +0530

    Created Post Card

--- client/src/components/Post/PostCard.jsx
+++ client/src/components/Post/PostCard.jsx
@@ -1,9 +1,69 @@
-import React from 'react'
+import React from "react";
+import {
+  Avatar,
+  Card,
+  CardActions,
+  CardContent,
+  CardHeader,
+  CardMedia,
+  IconButton,
+  Typography,
+} from "@mui/material";
+import FavoriteIcon from "@mui/icons-material/Favorite";
+import FavoriteBorderIcon from "@mui/icons-material/FavoriteBorder";
+import ShareIcon from "@mui/icons-material/Share";
+import { red } from "@mui/material/colors";
+import MoreVertIcon from "@mui/icons-material/MoreVert";
+import ChatBubbleIcon from "@mui/icons-material/ChatBubble";
+import BookmarkIcon from "@mui/icons-material/Bookmark";
+import BookmarkBorderIcon from "@mui/icons-material/BookmarkBorder";
 
 const PostCard = () => {
   return (
-    <div>PostCard</div>
-  )
-}
+    <Card className="">
+      <CardHeader
+        avatar={
+          <Avatar sx={{ bgcolor: red[500] }} aria-label="recipe">
+            R
+          </Avatar>
+        }
+        action={
+          <IconButton aria-label="settings">
+            <MoreVertIcon />
+          </IconButton>
+        }
+        title="Amaan Sayyed"
+        subheader="@Amaan63"
+      />
+      <CardMedia
+        component="img"
+        height="194"
+        image="https://cdn.pixabay.com/photo/2022/11/08/05/26/grilled-shrimp-7577730_1280.jpg"
+        alt="Paella dish"
+      />
+      <CardContent>
+        <Typography variant="body2" sx={{ color: "text.secondary" }}>
+          This impressive paella is a perfect party dish and a fun meal to cook
+          together with your guests. Add 1 cup of frozen peas along with the
+          mussels, if you like.
+        </Typography>
+      </CardContent>
+      <CardActions className="flex justify-between" disableSpacing>
+        <div>
+          <IconButton>
+            {true ? <FavoriteIcon /> : <FavoriteBorderIcon />}
+          </IconButton>
+          <IconButton>
+            <ShareIcon />
+          </IconButton>
+          <IconButton>
+            <ChatBubbleIcon />
+          </IconButton>
+        </div>
+        <div>{true ? <BookmarkIcon /> : <BookmarkBorderIcon />}</div>
+      </CardActions>
+    </Card>
+  );
+};
 
-export default PostCard
+export default PostCard;
