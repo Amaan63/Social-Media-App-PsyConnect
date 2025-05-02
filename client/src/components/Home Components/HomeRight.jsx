887f14da467ba65e0a16d8208dc9cb003e8b0e1f
Amaan63 <amaansayed63@gmail.com>
2025-05-02 20:53:19 +0530

    Imported teh Search User and Popular User in to the Home Right component

--- client/src/components/Home Components/HomeRight.jsx
+++ client/src/components/Home Components/HomeRight.jsx
@@ -1,9 +1,28 @@
-import React from 'react'
+import React from "react";
+import SearchUser from "../Search/SearchUser";
+import PopularUserCard from "./PopularUserCard";
+import { Card } from "@mui/material";
 
+const popularUser = [1, 2, 3, 4, 5, 6];
 const HomeRight = () => {
   return (
-    <div>HomeRight</div>
-  )
-}
+    <div className=" ">
+      <SearchUser />
+      <Card className=" m-5">
+        <div className="flex justify-between p-5  items-center">
+          <p className="font-semibold backdrop-opacity-70 ">
+            Suggestion for you
+          </p>
+          <p className="text-xs font-semibold  opacity-95">View All</p>
+        </div>
+        <div>
+          {popularUser.map((item) => (
+            <PopularUserCard key={item} />
+          ))}
+        </div>
+      </Card>
+    </div>
+  );
+};
 
-export default HomeRight
+export default HomeRight;
