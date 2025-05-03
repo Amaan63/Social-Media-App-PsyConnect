f03432a2219d7c31fd8f288f0bc30479a9f4bbc1
Amaan63 <amaansayed63@gmail.com>
2025-05-03 12:30:18 +0530

    Changed some styling and compatible with both Horizontal and Vertical videos

--- client/src/components/Reels/UserReelCard.jsx
+++ client/src/components/Reels/UserReelCard.jsx
@@ -2,11 +2,11 @@ import React from "react";
 
 const UserReelCard = () => {
   return (
-    <div className="w-[15rem] p-2">
+    <div className="w-[25rem] h-[30rem] p-2 flex items-center justify-center border-2 border-black rounded-lg shadow-lg">
       <video
         controls
-        className="w-full h-full"
-        src="https://cdn.pixabay.com/video/2022/07/20/124774-732319023_tiny.mp4"
+        className="max-w-full max-h-full object-contain"
+        src="https://cdn.pixabay.com/video/2025/01/03/250395_tiny.mp4"
       />
     </div>
   );
