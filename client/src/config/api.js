f523231ceb8ed6b49671474d90de17ebe47be944
Amaan63 <amaansayed63@gmail.com>
2025-05-03 13:24:41 +0530

    create Axios instance with baseURL and default headers

--- client/src/config/api.js
+++ client/src/config/api.js
@@ -1,1 +1,14 @@
+import axios from "axios";
+
 export const API_BASE_URL = import.meta.env.VITE_BACKEND_API_URL;
+
+const jwtToken = localStorage.getItem("jwt");
+
+export const api = axios.create({
+  baseURL: API_BASE_URL,
+  headers: {
+    Authorization: `Bearer ${jwtToken}`,
+    "Content-Type": "application/json",
+  },
+});
+
