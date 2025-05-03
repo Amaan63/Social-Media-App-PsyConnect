0bdd83b98bd760c763f9f2285963f2a609a3e302
Amaan63 <amaansayed63@gmail.com>
2025-05-03 14:54:04 +0530

    getting the user profile value from the action method

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,12 +3,21 @@ import "./App.css";
 import Authentication from "./Pages/Authentication/Authentication";
 import Message from "./Pages/MessagePage/Message";
 import Home from "./Pages/HomePage/Home";
+import { useDispatch, useSelector } from "react-redux";
+import { useEffect } from "react";
+import { getUserProfileAction } from "./Redux/Authentication/authentication.action";
 
 function App() {
+  const dispatch = useDispatch();
+  const jwt = localStorage.getItem("jwt");
+  const { auth } = useSelector((store) => store);
+  useEffect(() => {
+    dispatch(getUserProfileAction(jwt));
+  }, [jwt]);
   return (
     <>
       <Routes>
-        <Route path="/*" element={<Home />} />
+        <Route path="/*" element={auth.user ? <Home /> : <Authentication />} />
         <Route path="/message" element={<Message />} />
         {/* After / anything will come will go to Authentication */}
         <Route path="/*" element={<Authentication />} />
