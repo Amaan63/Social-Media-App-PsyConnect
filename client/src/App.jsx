2ddde23ed5a4174e204f736010ed65775ac18681
Amaan63 <amaansayed63@gmail.com>
2025-05-04 12:32:58 +0530

    Added Toast Container

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,6 +6,8 @@ import Home from "./Pages/HomePage/Home";
 import { useDispatch, useSelector } from "react-redux";
 import { useEffect } from "react";
 import { getUserProfileAction } from "./Redux/Authentication/authentication.action";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 function App() {
   const dispatch = useDispatch();
@@ -17,11 +19,12 @@ function App() {
   return (
     <>
       <Routes>
-        <Route path="/*" element={auth.user ? <Home /> : <Authentication />} />
+        <Route path="/home/*" element={<Home />} />
         {/* <Route path="/message" element={<Message />} /> */}
         {/* After / anything will come will go to Authentication */}
         <Route path="/*" element={<Authentication />} />
       </Routes>
+      <ToastContainer position="top-center" autoClose={3000} />
     </>
   );
 }
