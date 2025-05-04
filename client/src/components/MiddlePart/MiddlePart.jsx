d53e59df478b7f3be8376ae5e698963a8803a346
Amaan63 <amaansayed63@gmail.com>
2025-05-04 18:40:03 +0530

    Imported createPostModal component

--- client/src/components/MiddlePart/MiddlePart.jsx
+++ client/src/components/MiddlePart/MiddlePart.jsx
@@ -1,18 +1,25 @@
 import { Avatar, Card, IconButton } from "@mui/material";
-import React from "react";
+import React, { useState } from "react";
 import StoryCircle from "./StoryCircle";
 import AddIcon from "@mui/icons-material/Add";
 import ImageIcon from "@mui/icons-material/Image";
 import VideocamIcon from "@mui/icons-material/Videocam";
 import ArticleIcon from "@mui/icons-material/Article";
 import PostCard from "../Post/PostCard";
+import CreatePostModal from "../Post/CreatePostModal";
 
 const story = [11, 12, 13, 45, 69, 1];
 const posts = [1, 2, 3, 4, 5, 6];
 const MiddlePart = () => {
+  const [openCreatePostModal, setOpenCreatePostModal] = useState(false);
+
+  const handleCloseCreatePostModal = () => setOpenCreatePostModal(false);
+
   const handleOpenCreatePostModel = () => {
+    setOpenCreatePostModal(true);
     console.log("Open Post Model");
   };
+
   return (
     <div className="px-4 sm:px-8 md:px-20">
       <section className="flex items-center p-5 rounded-b-md overflow-x-auto">
@@ -28,32 +35,36 @@ const MiddlePart = () => {
       </section>
 
       <Card className="p-5 m-5">
-        <div className="flex justify-between items-center">
+        <div className="flex justify-between items-center cursor-pointer">
           <Avatar />
           <input
+            onClick={handleOpenCreatePostModel}
             readOnly
             className="outline-none w-full md:w-[90%] rounded-full px-5 bg-transparent border border-[#3b4054]"
             type="text"
             placeholder="What's on your mind?"
           />
         </div>
-        <div className="flex justify-center space-x-5 mt-5">
+        <div
+          className="flex justify-center space-x-5 mt-5"
+          onClick={handleOpenCreatePostModel}
+        >
           <div className="flex items-center">
-            <IconButton color="primary" onClick={handleOpenCreatePostModel}>
+            <IconButton color="primary">
               <ImageIcon />
             </IconButton>
             <span className="ml-2">Media</span>
           </div>
 
           <div className="flex items-center">
-            <IconButton color="primary" onClick={handleOpenCreatePostModel}>
+            <IconButton color="primary">
               <VideocamIcon />
             </IconButton>
             <span className="ml-2">Video</span>
           </div>
 
           <div className="flex items-center">
-            <IconButton color="primary" onClick={handleOpenCreatePostModel}>
+            <IconButton color="primary">
               <ArticleIcon />
             </IconButton>
             <span className="ml-2">Write Article</span>
@@ -65,6 +76,12 @@ const MiddlePart = () => {
         {posts.map((item) => (
           <PostCard key={item} />
         ))}
+      </div>
+      <div>
+        <CreatePostModal
+          handleClose={handleCloseCreatePostModal}
+          open={openCreatePostModal}
+        />
       </div>
     </div>
   );
