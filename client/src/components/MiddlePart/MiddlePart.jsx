39a3046740701d30b17fae2fc913752ff612d1d8
Amaan63 <amaansayed63@gmail.com>
2025-05-06 14:56:57 +0530

    Some minor changes

--- client/src/components/MiddlePart/MiddlePart.jsx
+++ client/src/components/MiddlePart/MiddlePart.jsx
@@ -27,7 +27,7 @@ const MiddlePart = () => {
 
   useEffect(() => {
     dispatch(getAllPostAction());
-  }, [post.newComment, post.post, post.comments]);
+  }, [post.newComment, post.post]);
 
   return (
     <div className="px-4 sm:px-8 md:px-20">
