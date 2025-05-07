fec86947609b14a31f0629debe32aeb8d8f5648a
Amaan63 <amaansayed63@gmail.com>
2025-05-07 20:46:42 +0530

    Created SearchUser staic component

--- client/src/components/Search/SearchUser.jsx
+++ client/src/components/Search/SearchUser.jsx
@@ -1,7 +1,43 @@
 import React from "react";
+import { Card } from "@mui/material";
+import { Avatar, CardHeader } from "@mui/material";
+import { grey, red } from "@mui/material/colors";
 
 const SearchUser = () => {
-  return <div>Search User</div>;
+  const handleSearchUser = () => {
+    console.log("Searched User:- ");
+  };
+  const handleClick = (id) => {
+    console.log(id);
+  };
+  return (
+    <div>
+      <div className="py-5 relative">
+        <input
+          className="bg-transparent border border-[#3b4054] outline-none w-full px-5 py-3 rounded-full"
+          type="text"
+          name="search"
+          id=""
+          placeholder="Search User......"
+          onChange={handleSearchUser}
+        />
+      </div>
+      {true && (
+        <Card sx={{ border: "1px solid", borderColor: grey[500] }}>
+          <CardHeader
+            onClick={() => {
+              handleClick(2);
+            }}
+            avatar={
+              <Avatar src="https://e7.pngegg.com/pngimages/799/987/png-clipart-computer-icons-avatar-icon-design-avatar-heroes-computer-wallpaper-thumbnail.png" />
+            }
+            title="Amaan Sayyed"
+            subheader={"amaansayyed63"}
+          />
+        </Card>
+      )}
+    </div>
+  );
 };
 
 export default SearchUser;
