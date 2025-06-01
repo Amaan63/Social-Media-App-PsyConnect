7bbeb3369611c16ba8a83b7cd1a6429c870668ea
Amaan63 <amaansayed63@gmail.com>
2025-06-01 13:14:02 +0530

    some styling changes

--- client/src/Pages/MessagePage/ChatMessage.jsx
+++ client/src/Pages/MessagePage/ChatMessage.jsx
@@ -2,13 +2,22 @@ import React from "react";
 
 const ChatMessage = () => {
   return (
-    <div>
+    <div
+      className={`flex ${true ? "justify-start" : "justify-end"} text-white`}
+    >
       <div
         className={`p-1 ${
           true ? "rounded-md" : "px-5 rounded-full"
-        } bg-[#191c29]`}
+        } bg-[#191c29] `}
       >
-        <p className="">Message</p>
+        {true && (
+          <img
+            className="w-[12rem] h-[17rem] object-cover rounded-md"
+            alt=""
+            src="https://cdn.pixabay.com/photo/2014/06/29/13/08/gosling-379393_1280.jpg"
+          />
+        )}
+        <p className={`${true ? "py-2" : "py-1"}`}>Message...</p>
       </div>
     </div>
   );
