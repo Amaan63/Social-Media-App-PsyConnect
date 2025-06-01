801d0eb1c60ccef814af6fcb342e9fd08d889fe2
Amaan63 <amaansayed63@gmail.com>
2025-06-01 13:15:09 +0530

    added username state

--- client/src/components/Search/SearchUser.jsx
+++ client/src/components/Search/SearchUser.jsx
@@ -1,14 +1,17 @@
-import React from "react";
+import React, { useState } from "react";
 import { Card, Avatar, CardHeader } from "@mui/material";
 import { grey } from "@mui/material/colors";
 import ErrorIcon from "@mui/icons-material/Error";
 
 const SearchUser = () => {
-  const handleSearchUser = () => {
+  const [username, setUsername] = useState("");
+  const handleSearchUser = (e) => {
+    setUsername(e.target.value);
     console.log("Searched User:- ");
   };
   const handleClick = (id) => {
     console.log(id);
+    setUsername("");
   };
   return (
     <div>
@@ -21,26 +24,30 @@ const SearchUser = () => {
           placeholder="Search User......"
           onChange={handleSearchUser}
         />
+        {username ? (
+          <Card
+            sx={{ border: "1px solid", borderColor: grey[500] }}
+            className="absolute w-full z-10 top-[4.5rem] cursor-pointer"
+          >
+            <CardHeader
+              onClick={() => {
+                handleClick(2);
+                setUsername("");
+              }}
+              avatar={
+                <Avatar src="https://e7.pngegg.com/pngimages/799/987/png-clipart-computer-icons-avatar-icon-design-avatar-heroes-computer-wallpaper-thumbnail.png" />
+              }
+              title="Amaan Sayyed"
+              subheader={"amaansayyed63"}
+            />
+          </Card>
+        ) : (
+          <div className="flex justify-center-safe gap-2  p-2 rounded">
+            <ErrorIcon className="text-red-500" />
+            <h3 className="text-base font-semibold">User Not Found</h3>
+          </div>
+        )}
       </div>
-      {false ? (
-        <Card sx={{ border: "1px solid", borderColor: grey[500] }}>
-          <CardHeader
-            onClick={() => {
-              handleClick(2);
-            }}
-            avatar={
-              <Avatar src="https://e7.pngegg.com/pngimages/799/987/png-clipart-computer-icons-avatar-icon-design-avatar-heroes-computer-wallpaper-thumbnail.png" />
-            }
-            title="Amaan Sayyed"
-            subheader={"amaansayyed63"}
-          />
-        </Card>
-      ) : (
-        <div className="flex justify-center-safe gap-2  p-2 rounded">
-          <ErrorIcon className="text-red-500" />
-          <h3 className="text-base font-semibold">User Not Found</h3>
-        </div>
-      )}
     </div>
   );
 };
