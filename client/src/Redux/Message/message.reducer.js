48cf7b5a0339b79bc2ebff3859504a862b950ee6
Amaan63 <amaansayed63@gmail.com>
2025-06-02 17:07:15 +0530

    added failure and request cases

--- client/src/Redux/Message/message.reducer.js
+++ client/src/Redux/Message/message.reducer.js
@@ -1,6 +1,12 @@
 import {
+  CREATE_CHAT_FAILURE,
+  CREATE_CHAT_REQUEST,
   CREATE_CHAT_SUCCESS,
+  CREATE_MESSAGE_FAILURE,
+  CREATE_MESSAGE_REQUEST,
   CREATE_MESSAGE_SUCCESS,
+  GET_ALL_CHATS_FAILURE,
+  GET_ALL_CHATS_REQUEST,
   GET_ALL_CHATS_SUCCESS,
 } from "./message.actionType";
 
@@ -14,6 +20,11 @@ const initialState = {
 
 export const messageReducer = (state = initialState, action) => {
   switch (action.type) {
+    case CREATE_MESSAGE_REQUEST:
+    case CREATE_CHAT_REQUEST:
+    case GET_ALL_CHATS_REQUEST:
+      return { ...state, error: null, loading: true };
+
     case CREATE_MESSAGE_SUCCESS:
       return { ...state, message: action.payload };
 
@@ -22,6 +33,11 @@ export const messageReducer = (state = initialState, action) => {
 
     case GET_ALL_CHATS_SUCCESS:
       return { ...state, chats: action.payload };
+
+    case CREATE_MESSAGE_FAILURE:
+    case CREATE_CHAT_FAILURE:
+    case GET_ALL_CHATS_FAILURE:
+      return { ...state, error: action.payload, loading: false };
 
     default:
       return state;
