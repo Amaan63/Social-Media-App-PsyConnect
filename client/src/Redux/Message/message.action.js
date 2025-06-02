745f5f8a27fe234d5bac51046f6371cc78c86744
Amaan63 <amaansayed63@gmail.com>
2025-06-02 14:31:15 +0530

    created message action

---
+++ client/src/Redux/Message/message.action.js
@@ -0,0 +1,51 @@
+import { api, API_BASE_URL } from "../../config/api";
+import {
+  CREATE_CHAT_FAILURE,
+  CREATE_CHAT_REQUEST,
+  CREATE_CHAT_SUCCESS,
+  CREATE_MESSAGE_FAILURE,
+  CREATE_MESSAGE_REQUEST,
+  CREATE_MESSAGE_SUCCESS,
+  GET_ALL_CHATS_FAILURE,
+  GET_ALL_CHATS_REQUEST,
+  GET_ALL_CHATS_SUCCESS,
+} from "./message.actionType";
+
+export const createMessageAction = (message) => async (dispatch) => {
+  dispatch({ type: CREATE_MESSAGE_REQUEST });
+  try {
+    const { data } = await api.post(
+      `/private/messages/createMessage/chat/{chatid}`,
+      message
+    );
+    console.log("Created Message", data);
+    dispatch({ type: CREATE_MESSAGE_SUCCESS, payload: data });
+  } catch (error) {
+    console.log("Message Error ", error);
+    dispatch({ type: CREATE_MESSAGE_FAILURE, payload: error });
+  }
+};
+
+export const createChatAction = (chat) => async (dispatch) => {
+  dispatch({ type: CREATE_CHAT_REQUEST });
+  try {
+    const { data } = await api.post(`/private/chats/createChat`, chat);
+    console.log("Created Chat", data);
+    dispatch({ type: CREATE_CHAT_SUCCESS, payload: data });
+  } catch (error) {
+    console.log("Chat Error ", error);
+    dispatch({ type: CREATE_CHAT_FAILURE, payload: error });
+  }
+};
+
+export const getAllChatsAction = (chat) => async (dispatch) => {
+  dispatch({ type: GET_ALL_CHATS_REQUEST });
+  try {
+    const { data } = await api.get(`/private/chats/findUsersChat`, chat);
+    console.log("Get All Chat", data);
+    dispatch({ type: GET_ALL_CHATS_SUCCESS, payload: data });
+  } catch (error) {
+    console.log("Get All Chat Error ", error);
+    dispatch({ type: GET_ALL_CHATS_FAILURE, payload: error });
+  }
+};
