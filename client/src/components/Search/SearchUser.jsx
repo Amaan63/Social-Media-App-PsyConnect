8c7df3e95d34c379188fb100da410fdf5ceb7fa7
Amaan63 <amaansayed63@gmail.com>
2025-06-02 17:09:56 +0530

    implemented user search

--- client/src/components/Search/SearchUser.jsx
+++ client/src/components/Search/SearchUser.jsx
@@ -2,52 +2,74 @@ import React, { useState } from "react";
 import { Card, Avatar, CardHeader } from "@mui/material";
 import { grey } from "@mui/material/colors";
 import ErrorIcon from "@mui/icons-material/Error";
+import { useDispatch, useSelector } from "react-redux";
+import { searchUserAction } from "../../Redux/Authentication/authentication.action";
+import { SEARCH_USER_SUCCESS } from "../../Redux/Authentication/authentication.actionType";
 
 const SearchUser = () => {
   const [username, setUsername] = useState("");
+  const dispatch = useDispatch();
+  const { auth } = useSelector((store) => store);
+
   const handleSearchUser = (e) => {
-    setUsername(e.target.value);
-    console.log("Searched User:- ");
+    const value = e.target.value;
+    setUsername(value);
+
+    if (value.trim() === "") {
+      dispatch({ type: SEARCH_USER_SUCCESS, payload: [] }); // Clear search
+    } else {
+      dispatch(searchUserAction(value));
+    }
   };
+
   const handleClick = (id) => {
-    console.log(id);
+    console.log("Selected User ID:", id);
     setUsername("");
   };
+
   return (
-    <div>
-      <div className="py-5 relative">
-        <input
-          className="bg-transparent border border-[#3b4054] outline-none w-full px-5 py-3 rounded-full"
-          type="text"
-          name="search"
-          id=""
-          placeholder="Search User......"
-          onChange={handleSearchUser}
-        />
-        {username ? (
-          <Card
-            sx={{ border: "1px solid", borderColor: grey[500] }}
-            className="absolute w-full z-10 top-[4.5rem] cursor-pointer"
-          >
-            <CardHeader
-              onClick={() => {
-                handleClick(2);
-                setUsername("");
-              }}
-              avatar={
-                <Avatar src="https://e7.pngegg.com/pngimages/799/987/png-clipart-computer-icons-avatar-icon-design-avatar-heroes-computer-wallpaper-thumbnail.png" />
-              }
-              title="Amaan Sayyed"
-              subheader={"amaansayyed63"}
-            />
-          </Card>
-        ) : (
-          <div className="flex justify-center-safe gap-2  p-2 rounded">
-            <ErrorIcon className="text-red-500" />
-            <h3 className="text-base font-semibold">User Not Found</h3>
-          </div>
-        )}
-      </div>
+    <div className="py-5 relative">
+      <input
+        className="bg-transparent border border-[#3b4054] outline-none w-full px-5 py-3 rounded-full"
+        type="text"
+        name="search"
+        placeholder="Search User......"
+        onChange={handleSearchUser}
+        value={username}
+      />
+
+      {/* Result container */}
+      {username && (
+        <div className="absolute w-full z-10 top-[4.5rem] bg-white max-h-60 overflow-y-auto rounded shadow">
+          {auth.searchUser.length > 0 ? (
+            auth.searchUser.map((item) => (
+              <Card
+                key={item.id}
+                sx={{
+                  border: "1px solid",
+                  borderColor: grey[500],
+                  marginBottom: "0.5rem",
+                }}
+                className="cursor-pointer"
+              >
+                <CardHeader
+                  onClick={() => handleClick(item.id)}
+                  avatar={
+                    <Avatar src="https://e7.pngegg.com/pngimages/799/987/png-clipart-computer-icons-avatar-icon-design-avatar-heroes-computer-wallpaper-thumbnail.png" />
+                  }
+                  title={`${item.firstName} ${item.lastName}`}
+                  subheader={`@${item.firstName}${item.lastName}`}
+                />
+              </Card>
+            ))
+          ) : (
+            <div className="flex items-center gap-2 p-2 bg-white border border-gray-300 rounded">
+              <ErrorIcon className="text-red-500" />
+              <h3 className="text-base font-semibold">User Not Found</h3>
+            </div>
+          )}
+        </div>
+      )}
     </div>
   );
 };
