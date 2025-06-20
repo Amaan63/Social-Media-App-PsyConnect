138fdf273a989a5115317649839edde36fbd1eb5
Amaan63 <amaansayed63@gmail.com>
2025-06-20 19:05:14 +0530

    handled the input and showing messages

--- client/src/Pages/MessagePage/Message.jsx
+++ client/src/Pages/MessagePage/Message.jsx
@@ -1,5 +1,11 @@
-import { Avatar, Grid, IconButton } from "@mui/material";
-import React, { useEffect } from "react";
+import {
+  Avatar,
+  Backdrop,
+  CircularProgress,
+  Grid,
+  IconButton,
+} from "@mui/material";
+import React, { useEffect, useState } from "react";
 import SearchUser from "../../components/Search/SearchUser";
 import WestIcon from "@mui/icons-material/West";
 import AddIcCallIcon from "@mui/icons-material/AddIcCall";
@@ -9,23 +15,55 @@ import { Link } from "react-router-dom";
 import UserChatCard from "./UserChatCard";
 import ChatMessage from "./ChatMessage";
 import { useDispatch, useSelector } from "react-redux";
-import { getAllChatsAction } from "../../Redux/Message/message.action";
+import {
+  createMessageAction,
+  getAllChatsAction,
+} from "../../Redux/Message/message.action";
+import ChatBubbleOutlineIcon from "@mui/icons-material/ChatBubbleOutline";
+import { uploadToCloudinary } from "../../utils/uploadToCloudinary";
 
 const Message = () => {
   const dispatch = useDispatch();
   const { auth, message } = useSelector((store) => store);
+  const [currentChat, setCurrentChat] = useState();
+  const [messages, setMessages] = useState([]);
+  const [selectedImage, setSelectedImage] = useState();
+  const [loading, setLoading] = useState(false);
+  const [inputValue, setInputValue] = useState(""); // 🔧 New state for message input
 
   useEffect(() => {
     dispatch(getAllChatsAction());
-  }, []);
+  }, [message.message]);
 
-  console.log("chats --------", message.chats);
+  console.log("chats --------", message);
 
-  const handleSelectImage = () => {
+  const handleSelectImage = async (e) => {
+    setLoading(true);
     console.log("Image Selected");
+    const imgUrl = await uploadToCloudinary(e.target.files[0], "image");
+    setSelectedImage(imgUrl);
+    setLoading(false);
   };
 
-  const handleCreateMessage = () => {};
+  const handleCreateMessage = () => {
+    if (!inputValue && !selectedImage) return; // Prevent sending empty messages
+
+    const message = {
+      chatId: currentChat?.id,
+      content: inputValue,
+      image: selectedImage,
+    };
+
+    console.log("Sending message:", message); // Optional debug log
+    dispatch(createMessageAction(message));
+
+    setInputValue(""); // Clear input after send
+    setSelectedImage(""); // Clear image after send
+  };
+
+  useEffect(() => {
+    setMessages([...messages, message.message]);
+  }, [message.message]);
 
   return (
     <div>
@@ -46,58 +84,111 @@ const Message = () => {
                   <SearchUser />
                 </div>
                 <div className="h-full space-y-4 mt-5 overflow-y-scroll hideScrollbar">
-                  {message.chats.map((item) => (
-                    <UserChatCard chat={item} />
-                  ))}
+                  {message.chats.map((item) => {
+                    return (
+                      <div
+                        onClick={() => {
+                          setCurrentChat(item);
+                          setMessages(item.messages);
+                        }}
+                      >
+                        <UserChatCard chat={item} />
+                      </div>
+                    );
+                  })}
                 </div>
               </div>
             </div>
           </div>
         </Grid>
         <Grid className="h-full" item xs={9}>
-          <div>
-            <div className="flex justify-between items-center border-l p-5">
-              <div className="flex items-center space-x-3">
-                <Avatar src="https://e7.pngegg.com/pngimages/799/987/png-clipart-computer-icons-avatar-icon-design-avatar-heroes-computer-wallpaper-thumbnail.png" />
-                <p>Amaan Sayyed</p>
+          {currentChat ? (
+            <div>
+              <div className="flex justify-between items-center border-l p-5">
+                <div className="flex items-center space-x-3">
+                  <Avatar src="https://e7.pngegg.com/pngimages/799/987/png-clipart-computer-icons-avatar-icon-design-avatar-heroes-computer-wallpaper-thumbnail.png" />
+                  <p>
+                    {auth.user.id == currentChat.users[0]?.id
+                      ? currentChat.users[1].firstName +
+                        " " +
+                        currentChat.users[1].lastName
+                      : currentChat.users[0].firstName +
+                        " " +
+                        currentChat.users[0].lastName}
+                  </p>
+                </div>
+                <div className="flex space-x-3">
+                  <IconButton>
+                    <AddIcCallIcon />
+                  </IconButton>
+                  <IconButton>
+                    <VideoCallIcon />
+                  </IconButton>
+                </div>
               </div>
-              <div className="flex space-x-3">
-                <IconButton>
-                  <AddIcCallIcon />
-                </IconButton>
-                <IconButton>
-                  <VideoCallIcon />
-                </IconButton>
+              <div className="hideScrollbar overflow-y-scroll h-[82vh] px-2 space-y-5 ">
+                {messages?.length > 0 ? (
+                  messages.map((item, index) => (
+                    <ChatMessage key={index} item={item} />
+                  ))
+                ) : (
+                  <p className="text-center text-gray-400 mt-10">
+                    No messages yet
+                  </p>
+                )}
+              </div>
+
+              <div className="sticky bottom-0 border-l">
+                {selectedImage && (
+                  <img
+                    className="w-[5rem] h-[5rem] object-cover px-2"
+                    src={selectedImage}
+                  />
+                )}
+                <div className="py-5 flex items-center justify-center space-x-5 ">
+                  <input
+                    value={inputValue}
+                    onChange={(e) => setInputValue(e.target.value)}
+                    onKeyPress={(e) => {
+                      if (e.key === "Enter") handleCreateMessage();
+                    }}
+                    className="bg-transparent border border-[#3b4054] rounded-full w-[90%] py-3 px-5"
+                    type="text"
+                    name="messages"
+                    placeholder="Type Message........."
+                  />
+
+                  <div>
+                    <input
+                      type="file"
+                      accept="image/*"
+                      onChange={handleSelectImage}
+                      className="hidden"
+                      id="image-input"
+                    />
+                    <label htmlFor="image-input">
+                      <AddPhotoAlternateIcon />
+                    </label>
+                  </div>
+                </div>
               </div>
             </div>
-            <div className="hideScrollbar overflow-y-scroll h-[82vh] px-2 space-y-5  border-l">
-              <ChatMessage />
+          ) : (
+            <div className="h-full space-y-5 flex flex-col justify-center items-center">
+              <p className="text-xl font-semibold">
+                <ChatBubbleOutlineIcon sx={{ fontSize: "15rem" }} />
+                No Chat Selected
+              </p>
             </div>
-          </div>
-          <div className="sticky bottom-0 border-l">
-            <div className="py-5 flex items-center justify-center space-x-5 ">
-              <input
-                className="bg-transparent border border-[#3b4054] rounded-full w-[90%] py-3 px-5"
-                type="text"
-                name="messages"
-                placeholder="Type Message........."
-              />
-              <div>
-                <input
-                  type="file"
-                  accept="image/*"
-                  onChange={handleSelectImage}
-                  className="hidden"
-                  id="image-input"
-                />
-                <label htmlFor="image-input">
-                  <AddPhotoAlternateIcon />
-                </label>
-              </div>
-            </div>
-          </div>
+          )}
         </Grid>
       </Grid>
+      <Backdrop
+        sx={(theme) => ({ color: "#fff", zIndex: theme.zIndex.drawer + 1 })}
+        open={loading}
+      >
+        <CircularProgress color="inherit" />
+      </Backdrop>
     </div>
   );
 };
